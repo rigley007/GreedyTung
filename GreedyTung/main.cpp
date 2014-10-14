5e3673e1973669ca1d22a7e08604eb3240b5a038
rigley007 <rui.ning.1990@gmail.com>
2014-10-13 20:59:36 -0500

    Initial Commit

---
+++ GreedyTung/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  GreedyTung
+//
+//  Created by Helen on 10/13/14.
+//  Copyright (c) 2014 ___IWINLAB___. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
